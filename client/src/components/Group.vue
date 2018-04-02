8e20c43c3e0f6d8dd36eecf68dbe29480099e739
seanburnham <sburnham92@marriottschool.byu.edu>
2018-04-02 12:16:00 -0600

    Created group page with current book and notification components

--- client/src/components/Group.vue
+++ client/src/components/Group.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>
-      <h1>Hello {{ this.$route.params.id }}</h1>
-  </div>
+  <v-layout row wrap text-xs-center>
+    <v-flex xs12>
+        <div style="margin-bottom: 1.5rem;">
+            <h1>Hello {{ this.$route.params.id }}</h1>
+        </div>
+    </v-flex>
+    <v-flex xs3 style="margin-left: 176px;" >
+        <group-current-reading/>
+    </v-flex>
+    <v-flex xs6>
+        <group-notifications/>
+    </v-flex>
+    </v-layout>
 </template>
 
+
+
 <script>
+
+    import GroupCurrentReading from './GroupCurrentReading.vue';
+    import GroupNotifications from './GroupNotifications.vue';
+
     export default {
     name: 'Group',
+    data () {
+    return {
     }
+  },
+  components: {
+      GroupCurrentReading,
+      GroupNotifications,
+
+  }
+}
 </script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/GroupCurrentReading.vue
@@ -0,0 +1,57 @@
+<template>
+
+    <v-card>
+        <v-toolbar style="background-color: #E8AB3C;" dark>
+          <v-toolbar-title>Currently Reading</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-title primary-title id="bookContent">
+            <div>
+                
+                <img class="bookPic" :src="thumbnail">
+                <h6 class="headline mb-0">{{title}}</h6>
+                <div>{{author}}</div>
+            </div>
+        </v-card-title>
+        <div id="progressBar">
+            Progress: {{valueDeterminate}}%
+            <v-progress-linear v-model="valueDeterminate" height="15"></v-progress-linear>
+        </div>
+    </v-card>
+
+</template>
+<script>
+
+export default {
+  name: "GroupCurrentReading",
+  data() {
+    return {
+        title: '1776',
+        thumbnail: 'static/images/1776.jpg',
+        author: 'David McCullough',
+        valueDeterminate: 50
+    };
+  },
+};
+</script>
+
+<style scoped>
+    .bookPic{
+        max-height: 200px;
+        border: 3px solid #fff;
+        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+        position: relative;
+    }
+
+    #bookContent{
+        justify-content: center;
+    }
+
+    #progressBar{
+        text-align: left;
+        max-width: 300px;
+        margin: 2rem;
+        padding-bottom: 25px;
+    }
+
+</style>
